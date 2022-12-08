6f1424124046e0c0e488a6c604fb4b737b4d9aea
Chris <cr1s@hotmail.it>
2022-12-08 18:21:54 +0100

    Add: exercise_Input

---
+++ src/PrintYourName.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PrintYourName {
+    public static void main(String[] args) {
+
+
+        String name = "Chris";
+        String surname = "Puhlisi";
+        System.out.printf("Hello my name is %s %s ",name,surname);
+
+    }
+}
